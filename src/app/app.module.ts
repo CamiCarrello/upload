934406543c52fdee6048ea1b38aa6281efc21a1d
Karoline Rocha <emaildakarolineribeiro@gmail.com>
2022-12-06 03:06:46 +0000

    Iniciando a Channel Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent} from './pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ChannelComponent } from './pages/channel/channel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ChannelComponent
   ],
   imports: [
     BrowserModule,
@@ -18,3 +20,4 @@ import { HomeComponent} from './pages/home/home.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
