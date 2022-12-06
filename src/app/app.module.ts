f100942eae445a7645c035f1c59aa2275ce2abd8
Karoline Rocha <emaildakarolineribeiro@gmail.com>
2022-12-06 03:16:22 +0000

    Alterando side bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ChannelComponent } from './pages/channel/channel.component';
+import { MenuSideBarComponent } from './components/menu-side-bar/menu-side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ChannelComponent
+    ChannelComponent,
+    MenuSideBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu-side-bar/menu-side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-side-bar',
+  templateUrl: './menu-side-bar.component.html',
+  styleUrls: ['./menu-side-bar.component.scss']
+})
+export class MenuSideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
